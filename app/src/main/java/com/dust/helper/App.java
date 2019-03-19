5bf36c4390ca180c369ed00de0f1e9ea09e3e11a
huichen <103123485@qq.com>
2019-03-19 09:26:51 +0800

    Initial commit

---
+++ app/src/main/java/com/dust/helper/App.java
@@ -0,0 +1,37 @@
+package com.dust.helper;
+
+import android.app.Application;
+import android.os.Build;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        disableAPIDialog();
+    }
+
+
+    /**
+     * 反射 禁止弹窗
+     */
+    private void disableAPIDialog(){
+        if (Build.VERSION.SDK_INT < 28)return;
+        try {
+            Class clazz = Class.forName("android.app.ActivityThread");
+            Method currentActivityThread = clazz.getDeclaredMethod("currentActivityThread");
+            currentActivityThread.setAccessible(true);
+            Object activityThread = currentActivityThread.invoke(null);
+            Field mHiddenApiWarningShown = clazz.getDeclaredField("mHiddenApiWarningShown");
+            mHiddenApiWarningShown.setAccessible(true);
+            mHiddenApiWarningShown.setBoolean(activityThread, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/dust/helper/MainActivity.java
@@ -0,0 +1,144 @@
+package com.dust.helper;
+
+import android.Manifest;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_CODE_ALERT_WINDOW = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView image = (ImageView)findViewById(R.id.image);
+
+
+        Glide.with(this).asGif().load(R.drawable.sun).listener(new RequestListener<GifDrawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                return false;
+            }
+        }).into(image);
+
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ObjectAnimator  goneAnimation = ObjectAnimator.ofFloat(view, "translationY", 0, 100);
+//        goneAnimation.addListener(CompositeFragment.this);
+//                ObjectAnimator alphaGoneAnimator = ObjectAnimator.ofFloat(view, "alpha", 1.0f, 0f);
+                ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(image,"rotation",0f,360f);
+
+                //隐藏动画集合
+                AnimatorSet goneSet = new AnimatorSet();
+                goneSet.setDuration(1000);
+                //添加动画
+                goneSet.play(goneAnimation).with(rotationAnimator);
+
+                goneSet.start();
+            }
+        });
+
+
+
+
+
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW},REQUEST_CODE_ALERT_WINDOW);
+
+        checkOverlayPermission();
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(AccessibilityUtil.checkAccessibility(MainActivity.this)) {
+
+                        tosSartService();
+                }
+            }
+        });
+    }
+
+    /**
+     * 开启服务
+     */
+    private void tosSartService() {
+        startService(
+                new Intent(MainActivity.this, TrackerService.class)
+                        .putExtra(TrackerService.COMMAND, TrackerService.COMMAND_OPEN)
+        );
+        finish();
+    }
+
+    private void checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Uri parse = Uri.parse("package:" + getPackageName());
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, parse);
+//                Intent intent = new Intent(Settings.TYPE_APPLICATION_OVERLAY, parse);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(intent, REQUEST_CODE);
+                Toast.makeText(this, "请先授予 \"Activity 栈\" 悬浮窗权限", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+//    //权限判断
+//        if (Build.VERSION.SDK_INT >= 23) {
+//        if(!Settings.canDrawOverlays(getApplicationContext())) {
+//            //启动Activity让用户授权
+//            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+//            startActivity(intent);
+//            return;
+//        } else {
+//            //执行6.0以上绘制代码
+//        }
+//    } else {
+//        //执行6.0以下绘制代码
+//    }
+
+
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && !Settings.canDrawOverlays(this)) {
+            checkOverlayPermission();
+        }
+
+//        if (requestCode == REQUEST_CODE_ALERT_WINDOW) {
+//            checkOverlayPermission();
+//
+//            Toast.makeText(this, "请先授予 \"Activity 栈\" 悬浮窗权限", Toast.LENGTH_LONG).show();
+//        }
+    }
+}
